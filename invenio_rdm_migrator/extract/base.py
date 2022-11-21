66479951cf1d6a9ec0d72d45562fc02f8c1b3900
Pablo Panero <pablo.panero@cern.ch>
2022-11-21 15:26:37 +0100

    global: base interfaces

---
+++ invenio_rdm_migrator/extract/base.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 CERN.
+#
+# Invenio-RDM-Migrator is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Invenio RDM migration extract interfaces."""
+
+
+from abc import ABC, abstractmethod
+
+
+class Extract(ABC):
+    """Base class for data extraction."""
+
+    @abstractmethod
+    def run(self):
+        """Yield one element at a time."""
+        pass
---
+++ invenio_rdm_migrator/load/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 CERN.
+#
+# Invenio-RDM-Migrator is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Invenio RDM migration load module."""
+
+
+__all__ = (
+    "Load",
+)
