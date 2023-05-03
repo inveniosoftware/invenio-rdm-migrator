bd88485a7cb3d09a69d2b802e0029082dc232188
Alex Ioannidis <a.ioannidis@cern.ch>
2023-05-03 20:12:22 +0200

    release: v1.0.0a7

--- invenio_rdm_migrator/__init__.py
+++ invenio_rdm_migrator/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2022 CERN.
+# Copyright (C) 2022-2023 CERN.
 #
 # Invenio-RDM-Migrator is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
@@ -8,6 +8,6 @@
 """InvenioRDM module for data migration."""
 
 
-__version__ = "1.0.0a6"
+__version__ = "1.0.0a7"
 
 __all__ = ("__version__",)
