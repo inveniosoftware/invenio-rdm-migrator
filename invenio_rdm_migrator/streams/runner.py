a42cf066279739306f8a9f83ad94e638dadf59f6
Pablo Panero <pablo.panero@cern.ch>
2023-05-04 17:06:53 +0200

    cache: cache load on start and dump on runner exception or end

--- invenio_rdm_migrator/streams/runner.py
+++ invenio_rdm_migrator/streams/runner.py
@@ -7,8 +7,11 @@
 
 """InvenioRDM migration streams runner."""
 
+from pathlib import Path
+
 import yaml
 
+from ..utils import ts
 from .cache import ParentsCache, RecordsCache
 from .streams import Stream
 
@@ -23,39 +26,46 @@ class Runner:
 
     def __init__(self, stream_definitions, config_filepath):
         """Constructor."""
+        config = self._read_config(config_filepath)
+        self.tmp_dir = Path(config.get("tmp_dir"))
+        self.tmp_dir.mkdir(parents=True, exist_ok=True)
+        self.cache_dir = Path(config.get("cache_dir"))
+        self.cache_dir.mkdir(parents=True, exist_ok=True)
+
         self.streams = []
         self.cache = {
-            # used to keep track of what Parent IDs we've already inserted in the PIDs table.
-            # {
-            #     '<parent_pid>': {
-            #         'id': <generated_parent_uuid>,
-            #         'version': {
-            #             'latest_index': 'record_index',
-            #             'latest_id': 'record id',
-            #         }
-            # }
-            "parents": ParentsCache(),
-            "records": RecordsCache(),
+            "parents": ParentsCache(filepath=self.cache_dir / "parents.json"),
+            "records": RecordsCache(filepath=self.cache_dir / "records.json"),
             "communities": {},
         }
-        config = self._read_config(config_filepath)
 
-        for definition in stream_definitions:
-            stream_config = config.get(definition.name)
-            if stream_config is not None:
-                # merge cache objects from stream definition config
-                stream_cache = stream_config.get("load", {}).pop("cache", {})
-                self.cache.update(stream_cache)
-                self.streams.append(
-                    Stream(
-                        definition.name,
-                        definition.extract_cls(**stream_config.get("extract", {})),
-                        definition.transform_cls(**stream_config.get("transform", {})),
-                        definition.load_cls(
-                            **stream_config.get("load", {}), cache=self.cache
-                        ),
+        try:
+            for definition in stream_definitions:
+                stream_config = config.get(definition.name)
+                if stream_config is not None:
+                    # merge cache objects from stream definition config
+                    stream_cache = stream_config.get("load", {}).pop("cache", {})
+                    self.cache.update(stream_cache)
+                    self.streams.append(
+                        Stream(
+                            definition.name,
+                            definition.extract_cls(**stream_config.get("extract", {})),
+                            definition.transform_cls(
+                                **stream_config.get("transform", {})
+                            ),
+                            definition.load_cls(
+                                **stream_config.get("load", {}),
+                                cache=self.cache,
+                                tmp_dir=self.tmp_dir,
+                            ),
+                        )
                     )
-                )
+        finally:
+            for name, cache in self.cache.items():
+                if name == "communities":  # FIXME: implement communities cache
+                    continue
+                cache_file = self.cache_dir / f"{name}.json"
+                cache.dump(cache_file)
 
     def run(self):
         """Run ETL streams."""
