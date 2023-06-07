931881a4cf1b71a55e00888464a0e8b5cac58742
Pablo Panero <pablo.panero@cern.ch>
2023-06-07 18:27:27 +0200

    state: replace json by sqlite

--- invenio_rdm_migrator/state/base.py
+++ invenio_rdm_migrator/state/base.py
@@ -6,7 +6,11 @@
 # it under the terms of the MIT License; see LICENSE file for more details.
 
 """Base state module."""
-import json
+
+import tempfile
+from io import StringIO
+
+from sqlalchemy import create_engine
 from abc import ABC, abstractmethod
 
 from ..logging import Logger
@@ -16,24 +20,31 @@ from ..utils import ts
 class State(ABC):
     """State interface."""
 
-    def __init__(self, filepath=None, validate=False):
+    def __init__(self, dbpath):
         """Constructor."""
-        self._data = {}
-        logger = Logger.get_logger()
-        if filepath and filepath.exists():  # load state from file
-            start = ts(iso=False)
-            logger.info(f"Loading state from {filepath} {ts()}")
-            with open(filepath, "r") as file:
-                self._data = json.loads(file.read())
 
-            logger.info(f"Finished loading state {ts()}")
-            if validate:
-                logger.info(f"Validating state {ts()}")
-                for _, entry in self._data.items():
-                    self._validate(entry)
-                logger.info(f"Finished validating state {ts()}")
-            end = ts(iso=False)
-            logger.info(f"State loading took {end-start} seconds.")
+        self.dbpath = dbpath
+
+        start = ts(iso=False)
+        logger = Logger.get_logger()
+        logger.info(f"Loading state from {dbpath} {ts()}")
+        # load file into memory
+        disk_file = create_engine(f"sqlite:///{dbpath}").connect()
+        # TODO: evaluate using ATTACH to avoid loading to a temporary file
+        tempfile = StringIO()
+        for line in disk_file.iterdump():
+            tempfile.write("%s\n" % line)
+        disk_file.close()
+        tempfile.seek(0)
+
+        # dump state into in-memory sqlite db
+        self.mem_state = create_engine(f"sqlite:///:memory:").connect()
+        self.mem_state.executescript(tempfile.read())
+        self.mem_state.commit()
+
+        end = ts(iso=False)
+        logger.info(f"Finished loading state {ts()}")
+        logger.info(f"State loading took {end-start} seconds.")
 
     def dump(self, filepath):
         """Dump state data into a json file."""
@@ -56,11 +67,6 @@ class State(ABC):
     def all(self):
         """Get all the data from the state."""
         return self._data.values()
-
-    @abstractmethod
-    def _validate(self, data):
-        """Validate data entry."""
-        pass
 
     def add(self, key, data):
         """Add key to the state.
