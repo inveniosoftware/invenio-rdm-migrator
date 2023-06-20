9be7559e406f6939b6fe79bde4e75f9a95f6ba27
Pablo Panero <pablo.panero@cern.ch>
2023-06-20 15:20:32 +0200

    transform: add identity key dict mixin

--- invenio_rdm_migrator/transform/identity.py
+++ invenio_rdm_migrator/transform/identity.py
@@ -7,6 +7,8 @@
 
 """Invenio RDM migration transform interfaces."""
 
+from functools import partial
+
 from .base import Transform
 
 
@@ -16,3 +18,33 @@ class IdentityTransform(Transform):
     def _transform(self, entry):
         """Transform entry."""
         yield entry
+
+
+class IdentityDictKeyMixin:
+    """Translates a function name to a dictionary key.
+
+    Prevents unnecessary implementations where the return value of a function
+    is merely the dictionary value of key with the same name as the function.
+    Or as in this case, prefixed with underscore.
+
+    .. highlight:: python
+    .. code-block:: python
+
+        def _test(data):
+            return data["test"]
+    """
+
+    @staticmethod
+    def unprefixed_dict_access(key, data):
+        """Removes the prefix from the key and returns the value of the dict."""
+        if key.startswith("_"):
+            key = key[1:]
+
+        return data[key]
+
+    def __getattr__(self, item):
+        """Fallback to the dict access when the attr/func was not found in the class."""
+        # note that python will give priority to the implemented functions before calling
+        # this method, the one called always is __getattribute__
+        # see test_identity_dict_key_mixin for more details
+        return partial(self.unprefixed_dict_access, item)
---
+++ tests/transform/test_identity.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2023 CERN.
+#
+# Invenio-RDM-Migrator is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Identity transformation tests."""
+
+import pytest
+
+from invenio_rdm_migrator.transform import (
+    IdentityDictKeyMixin,
+    IdentityTransform,
+    Transform,
+)
+
+
+@pytest.fixture(scope="module")
+def transform_with_mixin():
+    """Test instance of a transform class with identity mixin"""
+
+    class FixtureTransform(Transform, IdentityDictKeyMixin):
+        """Transform fixture class."""
+
+        def _implemented(self, entry):
+            return "foo"
+
+        def _transform(self, entry):
+            """Transform entry."""
+            return {
+                "implemented": self._implemented(entry),
+                "unimplemented": self._unimplemented(entry),
+            }
+
+    return FixtureTransform()
+
+
+def test_identity_dict_key_mixin(transform_with_mixin):
+    input = {"implemented": "test", "unimplemented": "test too"}
+    expected = {"implemented": "foo", "unimplemented": "test too"}
+
+    # assert the implemented method has priority over the dictionary value
+    # assert the unimplemented method falls back to the dictionary value
+    assert transform_with_mixin._transform(input) == expected
+
+
+def test_identity_transform():
+    input = {"implemented": "test", "unimplemented": "test too"}
+    expected = {"implemented": "test", "unimplemented": "test too"}
+
+    assert next(IdentityTransform()._transform(input)) == expected
