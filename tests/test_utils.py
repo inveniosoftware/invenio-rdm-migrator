068802ab53cf39414089ad878199f7c7da827dd0
Pablo Panero <pablo.panero@cern.ch>
2023-08-08 13:02:19 +0200

    utils: add missing test cases

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,7 +8,10 @@
 """Utils tests."""
 
 import json
+import re
 from uuid import UUID
+
+import pytest
 
 from invenio_rdm_migrator.utils import JSONEncoder, dict_set, ts
 
@@ -25,6 +28,12 @@ def test_current_timestamp():
     assert isinstance(ts(iso=False), float)
 
 
+def test_current_timestamp_fmt():
+    value = ts(fmt="%Y-%m-%dT%H:%M:%S")
+    exp = re.compile(r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}")
+    assert exp.match(value)
+
+
 ###
 # JSONEncoder
 ###
@@ -34,6 +43,16 @@ def test_no_uuid_values():
     data = {"key1": "value1", "key2": 2, "key3": [1, 2, 3]}
     encoded_data = JSONEncoder().encode(data)
     assert encoded_data == json.dumps(data)
+
+
+def test_unknown_values():
+    class Unknown:
+        """Needed to trigger the encoder type error."""
+
+        pass
+
+    data = {"key": Unknown()}
+    pytest.raises(TypeError, JSONEncoder().encode, data)
 
 
 def test_uuid_values():
